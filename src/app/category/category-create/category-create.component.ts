44242cbd860dd1380bc0f72b312ecc0ae89a1f56
serggiio <36702285+serggiio@users.noreply.github.com>
2023-05-05 02:01:45 -0400

    Add create category modal component

--- src/app/category/category-create/category-create.component.ts
+++ src/app/category/category-create/category-create.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Category } from 'src/app/core/models/Category';
+import { CategoryService } from 'src/app/core/service/category.service';
 
 @Component({
   selector: 'app-category-create',
   templateUrl: './category-create.component.html',
-  styleUrls: ['./category-create.component.css']
+  styleUrls: ['./category-create.component.css'],
 })
 export class CategoryCreateComponent {
+  categoryModel: Category = {};
+  closeResult = '';
 
+  @Input() categoryList?: Category[];
+
+  constructor(
+    private modalService: NgbModal,
+    private categoryService: CategoryService
+  ) {}
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        () => {}
+      );
+  }
+
+  createCategory() {
+    this.modalService.dismissAll();
+    this.categoryService.create({ entity: this.categoryModel }).subscribe({
+      next: (data) => {
+        this.categoryList?.push(this.categoryModel);
+        this.categoryModel = {};
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
